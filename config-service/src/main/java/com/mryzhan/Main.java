da8ab0e67992eff3c7596c7885000beac92c8ce4
ryzhan43 <ryzhan43@gmail.com>
2023-08-19 11:17:08 -0400

    Configurations service

--- config-service/src/main/java/com/mryzhan/Main.java
+++ config-service/src/main/java/com/mryzhan/Main.java
@@ -1,5 +1,10 @@
 package com.mryzhan;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
