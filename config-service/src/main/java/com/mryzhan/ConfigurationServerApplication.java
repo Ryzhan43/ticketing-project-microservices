78303ba9dc8791212afd066baf57fe4e1a2dba24
ryzhan43 <ryzhan43@gmail.com>
2023-08-19 13:45:21 -0400

    Configurations service

--- config-service/src/main/java/com/mryzhan/ConfigurationServerApplication.java
+++ config-service/src/main/java/com/mryzhan/ConfigurationServerApplication.java
@@ -1,12 +1,15 @@
 package com.mryzhan;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
 @SpringBootApplication
 @EnableConfigServer
-public class Main {
+public class ConfigurationServerApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigurationServerApplication.class,args);
     }
 }
